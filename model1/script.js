b1fbb5db8fed5fe41f5079ea089c2ec9bacf584f
StingerUA <101710505+StingerUA@users.noreply.github.com>
2025-03-20 03:37:33 +0300

    Update script.js

--- model1/script.js
+++ model1/script.js
@@ -1,13 +1,20 @@
-// Handles loading the events for <model-viewer>'s slotted progress bar
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+
+  if (updatingBar) {
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  }
+
   if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
+    progressBar?.classList.add('hide');
     event.target.removeEventListener('progress', onProgress);
   } else {
-    progressBar.classList.remove('hide');
+    progressBar?.classList.remove('hide');
   }
 };
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+const modelViewer = document.querySelector('model-viewer');
+if (modelViewer) {
+  modelViewer.addEventListener('progress', onProgress);
+}
